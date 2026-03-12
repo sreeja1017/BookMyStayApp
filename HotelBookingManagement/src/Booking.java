e4c12f521c502fb8c31339b99150a96ebd0b9f0e
unknown <manasvijhade0209@gmail.com>
2026-03-12 10:24:44 +0530

    created UC1

---
+++ HotelBookingManagement/src/Booking.java
@@ -0,0 +1,14 @@
+public class Booking {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     Welcome to Hotel Booking       ");
+        System.out.println("        System Version 1.0          ");
+        System.out.println("====================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Application execution completed.");
+    }
+}
+
